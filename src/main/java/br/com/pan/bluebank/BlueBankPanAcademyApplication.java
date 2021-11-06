b0a2553cf540e506d20792f7c2d51898702f98a5
Jonas Lucena <jonaslucena@outlook.com>
2021-11-05 22:10:16 -0300

    criação do projeto

---
+++ src/main/java/br/com/pan/bluebank/BlueBankPanAcademyApplication.java
@@ -0,0 +1,13 @@
+package br.com.pan.bluebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBankPanAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBankPanAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pan/bluebank/BlueBankPanAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pan.bluebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBankPanAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
