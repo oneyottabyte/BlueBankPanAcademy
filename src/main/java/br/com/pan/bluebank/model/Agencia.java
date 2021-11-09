85117471802f46c6b6f3981380d6a1a38209766e
celsobomfim28 <lufan20@gmail.com>
2021-11-08 22:37:56 -0300

    Criação da Classe Agencia e Realocação da Classe TipoMovimentacao para a package model/ENUM

---
+++ src/main/java/br/com/pan/bluebank/model/Agencia.java
@@ -0,0 +1,129 @@
+package br.com.pan.bluebank.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "tb_agencias")
+public class Agencia implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_agencia")
+    private Long id;
+    
+    @Pattern(regexp = "/^[0-9]+$/")
+    @NotBlank(message = "Número da agência é um atributo obrigatório!")   
+    @Column(name = "numero_agencia")
+    private String numeroAgencia;
+    
+    @Pattern(regexp = "/^[A-Za-z]+$/")
+    @NotBlank(message = "Nome da agência é um atributo obrigatório!")   
+    @Column(name = "nome_agencia")
+    private String nomeAgencia;
+    
+    @JoinColumn(name = "id_Gerente")
+    @OneToOne
+    private Gerente gerente;
+    
+    
+    public Agencia() {
+    }
+
+	public Agencia(
+			@Pattern(regexp = "/^[0-9]+$/") @NotBlank(message = "Número da agência é um atributo obrigatório!") String numeroAgencia,
+			@Pattern(regexp = "/^[A-Za-z]+$/") @NotBlank(message = "Nome da agência é um atributo obrigatório!") String nome,
+			Gerente gerente) {
+		super();
+		this.numeroAgencia = numeroAgencia;
+		this.nome = nome;
+		this.gerente = gerente;
+	}
+
+
+
+	public Agencia(
+			@Pattern(regexp = "/^[0-9]+$/") @NotBlank(message = "Número da agência é um atributo obrigatório!") String numeroAgencia,
+			@Pattern(regexp = "/^[A-Za-z]+$/") @NotBlank(message = "Nome da agência é um atributo obrigatório!") String nome) {
+		super();
+		this.numeroAgencia = numeroAgencia;
+		this.nome = nome;
+	}
+
+
+	public String getNumeroAgencia() {
+		return numeroAgencia;
+	}
+
+
+	public void setNumeroAgencia(String numeroAgencia) {
+		this.numeroAgencia = numeroAgencia;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public Gerente getGerente() {
+		return gerente;
+	}
+
+
+	public void setGerente(Gerente gerente) {
+		this.gerente = gerente;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agencia other = (Agencia) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+    
+    
+	
+
+}
