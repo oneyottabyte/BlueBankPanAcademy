c83040c20cb36fbc5cd95f13835e4052b75e0914
fhilips <fhilips.souza@gmail.com>
2021-11-13 15:06:41 -0300

    Criação do Cliente Service e Mapper

---
+++ src/main/java/br/com/pan/bluebank/services/ClienteService.java
@@ -0,0 +1,52 @@
+package br.com.pan.bluebank.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.pan.bluebank.dto.ClienteDTO;
+import br.com.pan.bluebank.mappers.ClienteMapper;
+import br.com.pan.bluebank.model.Cliente;
+import br.com.pan.bluebank.model.Endereco;
+import br.com.pan.bluebank.repositories.CidadeRepository;
+import br.com.pan.bluebank.repositories.ClienteRepository;
+import br.com.pan.bluebank.repositories.EnderecoRepository;
+import br.com.pan.bluebank.repositories.EstadoRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+			
+	public Cliente create(ClienteDTO dto) {	
+		Endereco endereco = enderecoRepository.save(dto.getEndereco());
+		dto.setEndereco(endereco);
+		Cliente entity = ClienteMapper.toEntity(dto);
+		return clienteRepository.save(entity);	
+	}
+	
+	public Cliente update(Long id, ClienteDTO dto) {
+		Cliente cliente = clienteRepository.findById(id).orElseThrow();
+		Cliente updatedEntity = ClienteMapper.updateEntity(cliente, dto);
+		
+		return clienteRepository.save(updatedEntity);
+	}
+	
+	public Cliente findById(Long id) {	
+		return clienteRepository.findById(id).orElseThrow();
+	}
+	
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}	
+	
+	public void delete(Long id) {
+		Cliente cliente = clienteRepository.findById(id).orElseThrow();
+		clienteRepository.delete(cliente);	
+	}
+}
