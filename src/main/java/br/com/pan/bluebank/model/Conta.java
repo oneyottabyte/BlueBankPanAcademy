c61b29b89ecda348da5a03c36adeb6e89cd217ac
Jonas Lucena <jonaslucena@outlook.com>
2021-11-13 15:21:45 -0300

    ajuste na classe Conta

--- src/main/java/br/com/pan/bluebank/model/Conta.java
+++ src/main/java/br/com/pan/bluebank/model/Conta.java
@@ -20,7 +20,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
-import javax.validation.constraints.Size
+import javax.validation.constraints.Size;
 import br.com.pan.bluebank.model.ENUM.TipoDeConta;
 
 @Entity
