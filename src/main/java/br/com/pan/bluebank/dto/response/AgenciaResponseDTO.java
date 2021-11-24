fdf0135536c16c47ac5b864412687d1a107f9915
oneyottabyte <junhinhodorian@gmail.com>
2021-11-23 21:11:28 -0300

    Criando Equals e Hashcode

--- src/main/java/br/com/pan/bluebank/dto/response/AgenciaResponseDTO.java
+++ src/main/java/br/com/pan/bluebank/dto/response/AgenciaResponseDTO.java
@@ -38,7 +38,42 @@ public class AgenciaResponseDTO {
 	public void setNomeGerente(String nomeGerente) {
 		this.nomeGerente = nomeGerente;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nomeAgencia == null) ? 0 : nomeAgencia.hashCode());
+		result = prime * result + ((nomeGerente == null) ? 0 : nomeGerente.hashCode());
+		result = prime * result + ((numeroAgencia == null) ? 0 : numeroAgencia.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AgenciaResponseDTO other = (AgenciaResponseDTO) obj;
+		if (nomeAgencia == null) {
+			if (other.nomeAgencia != null)
+				return false;
+		} else if (!nomeAgencia.equals(other.nomeAgencia))
+			return false;
+		if (nomeGerente == null) {
+			if (other.nomeGerente != null)
+				return false;
+		} else if (!nomeGerente.equals(other.nomeGerente))
+			return false;
+		if (numeroAgencia == null) {
+			if (other.numeroAgencia != null)
+				return false;
+		} else if (!numeroAgencia.equals(other.numeroAgencia))
+			return false;
+		return true;
+	}
 	
 }
