317bdb3044daaa7608bebfd6431d232dfaef96ea
Jonas Lucena <jonaslucena@outlook.com>
2021-11-25 21:09:36 -0300

    implementacao da classe AgenciaMapper e ajustes no service e controller

---
+++ src/main/java/br/com/pan/bluebank/mappers/AgenciaMapper.java
@@ -0,0 +1,14 @@
+package br.com.pan.bluebank.mappers;
+
+import br.com.pan.bluebank.dto.response.AgenciaResponseDTO;
+import br.com.pan.bluebank.model.Agencia;
+
+public class AgenciaMapper {
+		
+	public static AgenciaResponseDTO toDTO(Agencia agencia) {
+		return new AgenciaResponseDTO(
+				agencia.getNumeroAgencia(),
+				agencia.getNomeAgencia(),
+				agencia.getGerente().getNome());
+	}
+}
